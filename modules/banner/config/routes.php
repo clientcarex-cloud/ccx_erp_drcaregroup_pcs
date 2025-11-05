329af2a8714eded3f910eb3b9cf014fe114716ca
clientcarex-cloud <clientcarex@gmail.com>
2025-11-05 12:43:25 +0530

    removed some more modules

--- modules/banner/config/routes.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$route['banner/get_staff_by_branch'] = 'banner/get_staff_by_branch';
-
--- modules/banner/third_party/node.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-if (!defined('BN_REG_PROD_POINT')) {
-    define('BN_REG_PROD_POINT', 'https://api.corbitaltech.dev/api/v3/register');
-}
-if (!defined('BN_VAL_PROD_POINT')) {
-    define('BN_VAL_PROD_POINT', 'https://api.corbitaltech.dev/api/v3/validate');
-}
