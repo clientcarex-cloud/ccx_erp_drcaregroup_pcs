aa19b2fbcff1f32ebe7b74acd5076040a241e158
clientcarex-cloud <clientcarex@gmail.com>
2025-12-02 12:18:58 +0530

    removed more hide

--- application/helpers/my_functions_helper.php
+++ application/helpers/my_functions_helper.php
@@ -32,10 +32,33 @@ function my_filter_settings_sections()
         $newSections = [];
         if (isset($sections['general'])) {
             $newSections['general'] = $sections['general'];
+
+            // Filter children of general section
+            if (isset($newSections['general']['children'])) {
+                foreach ($newSections['general']['children'] as $key => $child) {
+                    if (in_array($child['id'], ['company', 'localization', 'email'])) {
+                        unset($newSections['general']['children'][$key]);
+                    }
+                }
+                // Re-index array to prevent issues
+                $newSections['general']['children'] = array_values($newSections['general']['children']);
+            }
         }
 
         $property->setValue($app, $newSections);
     } catch (Exception $e) {
         log_message('error', 'Failed to filter settings sections: ' . $e->getMessage());
     }
+}
+
+hooks()->add_action('app_admin_head', 'my_hide_settings_menu_items');
+
+function my_hide_settings_menu_items()
+{
+    echo '<style>
+        .settings-group-system-update,
+        .settings-group-system-info {
+            display: none !important;
+        }
+    </style>';
 }
