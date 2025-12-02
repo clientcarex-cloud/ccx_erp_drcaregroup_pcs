f44994c3708a4abbc1c294ee55f50b59bd2f9dfb
clientcarex-cloud <clientcarex@gmail.com>
2025-12-02 11:45:55 +0530

    only general

--- application/helpers/my_functions_helper.php
+++ application/helpers/my_functions_helper.php
@@ -9,3 +9,14 @@ function my_change_default_url_to_admin(){
         redirect(site_url('admin/authentication'));
     }
 }
+
+hooks()->add_filter('settings_tabs', 'my_filter_settings_tabs');
+
+function my_filter_settings_tabs($tabs){
+    foreach($tabs as $key => $tab){
+        if($key != 'general'){
+            unset($tabs[$key]);
+        }
+    }
+    return $tabs;
+}
