5db17fb7af8cd2ba0304094fe2d8065bcb0a5527
clientcarex-cloud <clientcarex@gmail.com>
2026-02-23 15:34:54 +0530

    reorder fields

--- modules/client/views/reports/doctor_appointments.php
+++ modules/client/views/reports/doctor_appointments.php
@@ -44,8 +44,26 @@
                             }
                             ?>
 
+                            <div class="col-md-3">
+                                <label for="consulted_date">
+                                    <?= _l('from_date'); ?> *
+                                </label>
+                                <input type="date" class="form-control" name="consulted_date" id="consulted_date"
+                                    value="<?= date('Y-m-d'); ?>" required>
+                            </div>
+                            <div class="col-md-3">
+                                <label for="consulted_to_date">
+                                    <?= _l('to_date'); ?> *
+                                </label>
+                                <input type="date" class="form-control" name="consulted_to_date" id="consulted_to_date"
+                                    value="<?= date('Y-m-d'); ?>" required>
+                            </div>
+                            <div class="col-md-3">
+                                <?= render_select('appointment_type_id', $appointment_type, ['appointment_type_id', 'appointment_type_name'], _l('appointment_type') . '*', '', ['data-none-selected-text' => _l('dropdown_non_selected_tex'), 'required' => 'required']) ?>
+                            </div>
+
                             <?php if (count($final_doctor_list) > 0 && count($final_doctor_list) != 1): ?>
-                                <div class="col-md-4">
+                                <div class="col-md-3">
                                     <?= render_select(
                                         'appointment_branch_id',
                                         $branch,
@@ -58,6 +76,11 @@
                                         ['id' => 'appointment_branch_id', 'required' => 'required', 'data-none-selected-text' => _l('dropdown_non_selected_tex')]
                                     ) ?>
                                 </div>
+                            <?php endif; ?>
+                        </div>
+
+                        <div class="row">
+                            <?php if (count($final_doctor_list) > 0 && count($final_doctor_list) != 1): ?>
                                 <div class="col-md-4">
                                     <?= render_select(
                                         'enquiry_doctor_id',
@@ -83,27 +106,8 @@
                                     <option value="Visited">Visited</option>
                                 </select>
                             </div>
-                        </div>
 
-                        <div class="row">
-                            <div class="col-md-3">
-                                <label>
-                                    <?= _l('from_date'); ?>
-                                </label>
-                                <input type="date" class="form-control" name="consulted_date" id="consulted_date"
-                                    value="<?= date('Y-m-d'); ?>">
-                            </div>
-                            <div class="col-md-3">
-                                <label>
-                                    <?= _l('to_date'); ?>
-                                </label>
-                                <input type="date" class="form-control" name="consulted_to_date" id="consulted_to_date"
-                                    value="<?= date('Y-m-d'); ?>">
-                            </div>
-                            <div class="col-md-3">
-                                <?= render_select('appointment_type_id', $appointment_type, ['appointment_type_id', 'appointment_type_name'], _l('appointment_type') . '*', '', ['data-none-selected-text' => _l('dropdown_non_selected_tex'), 'required' => 'required']) ?>
-                            </div>
-                            <div class="col-md-2 d-flex align-items-end" style="margin-top: 26px;">
+                            <div class="col-md-4 d-flex align-items-end" style="margin-top: 26px;">
                                 <button id="searchAppointmentsBtn" class="btn btn-success w-100">
                                     <?= _l('Search'); ?>
                                 </button>
@@ -134,7 +138,8 @@
 
 <?php init_tail(); ?>
 
-<?php if (isset($client_modal)) echo $client_modal; ?>
+<?php if (isset($client_modal))
+    echo $client_modal; ?>
 
 <script>
     $(document).ready(function () {
@@ -146,35 +151,47 @@
             });
         <?php endif; ?>
 
-        let from = $('#consulted_date').val();
-        let to = $('#consulted_to_date').val();
-        let enquiry_doctor_id = $('#enquiry_doctor_id').val() || '0';
-        let appointment_type_id = $('#appointment_type_id').val() || '0';
-        let branch_id = $('#appointment_branch_id').val() || '0';
-
-
-        var url = `<?= admin_url("client/reports/doctor_appointments/null/") ?>${from}/${to}/${enquiry_doctor_id}/All/${branch_id}/0/${appointment_type_id}`;
-
-        var table = initDataTable(
-            '.table-doctor-appointments',
-            url,
-            [1],
-            [1]
-        );
+        let table_initialized = false;
 
         $('#searchAppointmentsBtn').click(function () {
-            from = $('#consulted_date').val();
-            to = $('#consulted_to_date').val();
-            enquiry_doctor_id = $('#enquiry_doctor_id').val() || '0';
-            appointment_type_id = $('#appointment_type_id').val() || '0';
-            branch_id = $('#appointment_branch_id').val() || '0';
+            let from = $('#consulted_date').val();
+            let to = $('#consulted_to_date').val();
+            let appointment_type_id = $('#appointment_type_id').val();
+            let branch_id = $('#appointment_branch_id').val();
+            let enquiry_doctor_id = $('#enquiry_doctor_id').val() || '0';
+
+            // Check mandatory fields
+            if (!from || !to) {
+                alert_float('warning', 'Please select Date Range');
+                return;
+            }
+            if (!appointment_type_id) {
+                alert_float('warning', 'Please select Appointment Type');
+                return;
+            }
+            if ($('#appointment_branch_id').length > 0 && !branch_id) {
+                alert_float('warning', 'Please select Branch');
+                return;
+            }
+
+            appointment_type_id = appointment_type_id || '0';
+            branch_id = branch_id || '0';
 
             let visit_status = $('#appointment_status option:selected').text().trim();
             visit_status = visit_status && visit_status !== 'Select Response' ? visit_status.replace(/\s+/g, '_') : 'All';
 
-            if ($.fn.DataTable.isDataTable('.table-doctor-appointments')) {
-                const newUrl = `<?= admin_url("client/reports/doctor_appointments/null/") ?>${from}/${to}/${enquiry_doctor_id}/${visit_status}/${branch_id}/0/${appointment_type_id}`;
+            const newUrl = `<?= admin_url("client/reports/doctor_appointments/null/") ?>${from}/${to}/${enquiry_doctor_id}/${visit_status}/${branch_id}/0/${appointment_type_id}`;
+
+            if (table_initialized) {
                 $('.table-doctor-appointments').DataTable().ajax.url(newUrl).load();
+            } else {
+                initDataTable(
+                    '.table-doctor-appointments',
+                    newUrl,
+                    [1],
+                    [1]
+                );
+                table_initialized = true;
             }
         });
     });
