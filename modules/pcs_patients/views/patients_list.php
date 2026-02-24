fa3facb6d3a42c32aa38b3dc4716a869171a3202
clientcarex-cloud <clientcarex@gmail.com>
2026-02-24 12:00:53 +0530

    ordering changed

--- modules/pcs_patients/views/patients_list.php
+++ modules/pcs_patients/views/patients_list.php
@@ -62,7 +62,7 @@
 <?php init_tail(); ?>
 <script>
     $(function () {
-        var patientsTable = initDataTable('.table-patients', '<?= admin_url('client/get_patient_list'); ?>', [0], [0]);
+        var patientsTable = initDataTable('.table-patients', '<?= admin_url('client/get_patient_list'); ?>', [0], [0], 'undefined', [0, 'desc']);
         if (!patientsTable || !patientsTable.on) {
             patientsTable = $('.table-patients').DataTable();
         }
